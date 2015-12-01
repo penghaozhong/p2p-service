ed0c4f5286b42787f835ae763f30449df389e42a
penghaozhong <penghaozhong@163.com>
2015-12-01 20:58:57 +0800

    调试系统连通

--- src/main/java/com/lcsuo/p2p/service/action/TestAction.java
+++ src/main/java/com/lcsuo/p2p/service/action/TestAction.java
@@ -1,5 +1,17 @@
 package com.lcsuo.p2p.service.action;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestAction {
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@ResponseBody
+	public String test() {
+		return "test";
+	}
 
 }
